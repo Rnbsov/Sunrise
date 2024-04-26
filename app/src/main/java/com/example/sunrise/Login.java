84c5ae9e5ddf8ed1495656b6d245651c5ab89f15
Rnbsov <rnbsov@gmail.com>
2024-04-26 16:58:29 +0500

    🍉 Login logic

--- app/src/main/java/com/example/sunrise/Login.java
+++ app/src/main/java/com/example/sunrise/Login.java
@@ -1,14 +1,33 @@
 package com.example.sunrise;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Login extends AppCompatActivity {
+
+    TextInputEditText editTextEmail, editTextPassword;
+    Button loginBtn;
+    TextView registerScreen;
+    private FirebaseAuth mAuth;
+    private static final String TAG = "LoginActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +38,50 @@ public class Login extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Initialize Firebase Auth object
+        mAuth = FirebaseAuth.getInstance();
+
+        // Getting views
+        editTextEmail = findViewById(R.id.email);
+        editTextPassword = findViewById(R.id.password);
+        loginBtn = findViewById(R.id.login_btn);
+
+        loginBtn.setOnClickListener(v -> {
+            String email, password;
+
+            email = String.valueOf(editTextEmail.getText());
+            password = String.valueOf(editTextPassword.getText());
+
+            if (TextUtils.isEmpty(email)) {
+                Toast.makeText(this, "Enter email", Toast.LENGTH_SHORT).show();
+            }
+
+            if (TextUtils.isEmpty(password)) {
+                Toast.makeText(this, "Enter password", Toast.LENGTH_SHORT).show();
+            }
+
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success
+                                Log.d(TAG, "signInWithEmail:success");
+                                Toast.makeText(Login.this, "Login success",
+                                        Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                startActivity(intent);
+                                finish();
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                Toast.makeText(Login.this, "Authentication failed",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
         });
     }
 }
