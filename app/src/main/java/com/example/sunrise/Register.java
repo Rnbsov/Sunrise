73bac8e35a82a30304fdc42501f6552abe2003ee
Rnbsov <rnbsov@gmail.com>
2024-04-26 16:31:35 +0500

    🍊 Registration logic

--- app/src/main/java/com/example/sunrise/Register.java
+++ app/src/main/java/com/example/sunrise/Register.java
@@ -1,14 +1,30 @@
 package com.example.sunrise;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class Register extends AppCompatActivity {
+    TextInputEditText editTextEmail, editTextPassword;
+    Button registerBtn;
+    private FirebaseAuth mAuth;
+    private static final String TAG = "RegisterActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +35,47 @@ public class Register extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Initialize Firebase Auth object
+        mAuth = FirebaseAuth.getInstance();
+
+        // Getting views
+        editTextEmail = findViewById(R.id.email);
+        editTextPassword = findViewById(R.id.password);
+        registerBtn = findViewById(R.id.register_btn);
+
+        registerBtn.setOnClickListener(v -> {
+            String email, password;
+
+            email = String.valueOf(editTextEmail.getText());
+            password = String.valueOf(editTextPassword.getText());
+
+            if (TextUtils.isEmpty(email)) {
+                Toast.makeText(this, "Enter email", Toast.LENGTH_SHORT).show();
+            }
+
+            if (TextUtils.isEmpty(password)) {
+                Toast.makeText(this, "Enter password", Toast.LENGTH_SHORT).show();
+            }
+
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success
+                                Log.d(TAG, "createUserWithEmail:success");
+                                Toast.makeText(Register.this, "Account created!",
+                                        Toast.LENGTH_SHORT).show();
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                Toast.makeText(Register.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
         });
     }
 }
