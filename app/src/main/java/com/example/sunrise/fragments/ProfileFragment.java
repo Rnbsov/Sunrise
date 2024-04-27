6062d5ce252bf1f2520dab776be7992f2055b428
Rnbsov <rnbsov@gmail.com>
2024-04-27 22:41:53 +0500

    🍉 Sign out functionality

--- app/src/main/java/com/example/sunrise/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/sunrise/fragments/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.example.sunrise.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,60 +8,49 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.example.sunrise.Login;
 import com.example.sunrise.R;
+import com.google.firebase.auth.FirebaseAuth;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ProfileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    Button signOutBtn;
+    private FirebaseAuth mAuth;
 
     public ProfileFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        // Initialize Firebase Auth object
+        mAuth = FirebaseAuth.getInstance();
+
+        signOutBtn = view.findViewById(R.id.sign_out_btn);
+
+        signOutBtn.setOnClickListener(v -> {
+            FirebaseAuth.getInstance().signOut();
+
+            // Send user to Login activity
+            Intent intent = new Intent(getContext(), Login.class);
+            startActivity(intent);
+
+            // Finish the parent of fragments which is MainActivity
+            if (getActivity() != null) {
+                getActivity().finish();
+            }
+
+            // Notify user about successful sign out
+            Toast.makeText(getContext(), "Sign out success", Toast.LENGTH_SHORT).show();
+        });
+
+        return view;
     }
 }
