2156a956b355b30a73bb56562b3f645608f037a7
Rnbsov <rnbsov@gmail.com>
2024-04-29 13:35:17 +0500

    🍃🍊 Method for retrieving tasks from Firebase

--- app/src/main/java/com/example/sunrise/services/TaskService.java
+++ app/src/main/java/com/example/sunrise/services/TaskService.java
@@ -1,10 +1,13 @@
 package com.example.sunrise.services;
 
 import com.example.sunrise.models.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
 
 public class TaskService {
 
@@ -30,5 +33,19 @@ public class TaskService {
         task.setTaskId(taskId); // Save this unique ID to the task object
 
         newTaskRef.setValue(task); // Save the task to Firebase database
+    }
+
+    /**
+     * Method to retrieve all tasks of currently logged-in user
+     */
+    public void getTasks(ValueEventListener listener) {
+        // Get currently logged-in user Id
+        String userId = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
+
+        // Create a query to filter tasks by createdByUserId
+        Query query = tasksRef.orderByChild("createdByUserId").equalTo(userId);
+
+        // Add ValueEventListener to the query to listen for changes in Firebase data
+        query.addValueEventListener(listener);
     }
 }
