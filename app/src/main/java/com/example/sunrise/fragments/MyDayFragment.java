1b2fc4309e74a0b30a9dac88a9ad386757b5d122
Rnbsov <rnbsov@gmail.com>
2024-04-29 13:24:51 +0500

    ♻️ Make use of RecyclerView inside of MyDayFragment

--- app/src/main/java/com/example/sunrise/fragments/MyDayFragment.java
+++ app/src/main/java/com/example/sunrise/fragments/MyDayFragment.java
@@ -2,13 +2,19 @@ package com.example.sunrise.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.example.sunrise.R;
+import com.example.sunrise.adapters.TasksAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +22,8 @@ import com.example.sunrise.R;
  * create an instance of this fragment.
  */
 public class MyDayFragment extends Fragment {
+    private View fragment;
+    private RecyclerView tasksList;
 
     public MyDayFragment() {
         // Required empty public constructor
@@ -25,7 +33,23 @@ public class MyDayFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_my_day, container, false);
-        return v;
+        fragment = inflater.inflate(R.layout.fragment_my_day, container, false);
+
+        return fragment;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        tasksList = fragment.findViewById(R.id.tasks_list);
+
+        // Creating and setting linear layout manager to recyclerView
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        tasksList.setLayoutManager(layoutManager);
+
+        // Setting custom adapter to recyclerView
+        TasksAdapter adapter = new TasksAdapter();
+        tasksList.setAdapter(adapter);
     }
 }
