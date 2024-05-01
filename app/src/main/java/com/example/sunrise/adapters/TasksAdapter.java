84502e25a5b1dd700ec46da2c91aa911708686ec
Rnbsov <rnbsov@gmail.com>
2024-05-01 17:19:21 +0500

    🍐 Rename some members of TaskAdapter

--- app/src/main/java/com/example/sunrise/adapters/TasksAdapter.java
+++ app/src/main/java/com/example/sunrise/adapters/TasksAdapter.java
@@ -17,10 +17,10 @@ import com.google.android.material.chip.Chip;
 
 import java.util.List;
 
-public class TasksAdapter extends RecyclerView.Adapter<TasksAdapter.ViewHolder> {
+public class TasksAdapter extends RecyclerView.Adapter<TasksAdapter.TaskViewHolder> {
 
     private static List<Task> localDataSet;
-    private final onCheckboxClickedListener onCheckedChangeListener;
+    private final OnTaskCheckedChangeListener onCheckedChangeListener;
 
     /**
      * Initialize the dataset of the Adapter
@@ -28,29 +28,29 @@ public class TasksAdapter extends RecyclerView.Adapter<TasksAdapter.ViewHolder> 
      * @param dataSet String[] containing the data to populate views to be used
      * by RecyclerView
      */
-    public TasksAdapter(List<Task> dataSet, onCheckboxClickedListener onCheckedChangeListener) {
+    public TasksAdapter(List<Task> dataSet, OnTaskCheckedChangeListener onCheckedChangeListener) {
         localDataSet = dataSet;
         this.onCheckedChangeListener = onCheckedChangeListener;
     }
 
-    public interface onCheckboxClickedListener {
+    public interface OnTaskCheckedChangeListener {
         void onCompleted(Task task, TextView title, boolean isChecked);
     }
 
     // Create new views (invoked by the layout manager)
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+    public TaskViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
         // Create a new view, which defines the UI of the list item
         View view = LayoutInflater.from(viewGroup.getContext())
                 .inflate(R.layout.task_item_layout, viewGroup, false);
 
-        return new ViewHolder(view, onCheckedChangeListener);
+        return new TaskViewHolder(view, onCheckedChangeListener);
     }
 
     // Replace the contents of a view (invoked by the layout manager)
     @Override
-    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+    public void onBindViewHolder(TaskViewHolder viewHolder, final int position) {
         // Get element from your dataset at this position and replace the
         // contents of the view with that element
         Task task = localDataSet.get(position);
@@ -91,12 +91,12 @@ public class TasksAdapter extends RecyclerView.Adapter<TasksAdapter.ViewHolder> 
      * Provide a reference to the type of views that you are using
      * (custom ViewHolder)
      */
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public static class TaskViewHolder extends RecyclerView.ViewHolder {
         private final TextView title;
         private final CheckBox completeCheckbox;
         private final Chip priority;
 
-        public ViewHolder(View itemView, onCheckboxClickedListener onCheckboxClick) {
+        public TaskViewHolder(View itemView, OnTaskCheckedChangeListener onCheckboxClick) {
             super(itemView);
             // TODO: Define click listener for the ViewHolder's View
 
