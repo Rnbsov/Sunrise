bbd188d4094963cf397adc707ac5288817cb1c8d
Rnbsov <rnbsov@gmail.com>
2024-05-04 19:30:48 +0500

    🍐 Just some renaming

--- app/src/main/java/com/example/sunrise/services/TagService.java
+++ app/src/main/java/com/example/sunrise/services/TagService.java
@@ -1,6 +1,6 @@
 package com.example.sunrise.services;
 
-import com.example.sunrise.models.Task;
+import com.example.sunrise.models.Tag;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -11,53 +11,53 @@ import java.util.Objects;
 
 public class TagService {
 
-    private final DatabaseReference tasksRef;
+    private final DatabaseReference tagsRef;
 
     public TagService() {
         // Initialize Firebase database reference
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        tasksRef = database.getReference("Tags");
+        tagsRef = database.getReference("Tags");
     }
 
     /**
      * Method to save the tag to Firebase database
      */
-    public void saveTask(Task task) {
+    public void saveTag(Tag tag) {
         // Update the updatedAt timestamp before starting saving
-        task.setUpdatedAt(System.currentTimeMillis());
+        tag.setUpdatedAt(System.currentTimeMillis());
 
         // Generate a reference to a new child location under "Tags" with a client-side auto-generated key
-        DatabaseReference newTaskRef = tasksRef.push();
+        DatabaseReference newTagRef = tagsRef.push();
 
-        String taskId = newTaskRef.getKey(); // Retrieve the unique ID
-        task.setTaskId(taskId); // Save this unique ID to the tag object
+        String tagId = newTagRef.getKey(); // Retrieve the unique ID
+        tag.setTagId(tagId); // Save this unique ID to the tag object
 
-        newTaskRef.setValue(task); // Save the tag to Firebase database
+        newTagRef.setValue(tag); // Save the tag to Firebase database
     }
 
     /**
      * Method to update the tag in Firebase database
      */
-    public void updateTask(Task task) {
+    public void updateTag(Tag tag) {
         // Update the updatedAt timestamp before starting saving
-        task.setUpdatedAt(System.currentTimeMillis());
+        tag.setUpdatedAt(System.currentTimeMillis());
 
         // Get the reference to the tag's location in Firebase using its unique ID
-        DatabaseReference taskRef = tasksRef.child(task.getTaskId());
+        DatabaseReference tagRef = tagsRef.child(tag.getTagId());
 
         // Update the tag at the specified location in Firebase
-        taskRef.setValue(task);
+        tagRef.setValue(tag);
     }
 
     /**
      * Method to retrieve all tasks of currently logged-in user
      */
-    public void getTasks(ValueEventListener listener) {
+    public void getTags(ValueEventListener listener) {
         // Get currently logged-in user Id
         String userId = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
 
         // Create a query to filter tags by createdByUserId
-        Query query = tasksRef.orderByChild("createdByUserId").equalTo(userId);
+        Query query = tagsRef.orderByChild("createdByUserId").equalTo(userId);
 
         // Add ValueEventListener to the query to listen for changes in Firebase data
         query.addValueEventListener(listener);
