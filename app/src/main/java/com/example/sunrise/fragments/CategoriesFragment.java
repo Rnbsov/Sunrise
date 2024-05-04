9fc0a0db53ba8ff9b80d5ba95328dd6b44500446
Rnbsov <rnbsov@gmail.com>
2024-05-04 15:47:14 +0500

    🍐 Navigate to Tags fragment listener

--- app/src/main/java/com/example/sunrise/fragments/CategoriesFragment.java
+++ app/src/main/java/com/example/sunrise/fragments/CategoriesFragment.java
@@ -2,15 +2,25 @@ package com.example.sunrise.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.sunrise.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.Objects;
 
 public class CategoriesFragment extends Fragment {
+
+    private View fragment;
 
     public CategoriesFragment() {
         // Required empty public constructor
@@ -25,6 +35,22 @@ public class CategoriesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_categories, container, false);
+        fragment = inflater.inflate(R.layout.fragment_categories, container, false);
+        return fragment;}
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        CardView tagsTile = fragment.findViewById(R.id.tags_tile);
+
+        tagsTile.setOnClickListener(v -> {
+            // Get the NavController
+            NavController navController = Navigation.findNavController(view);
+
+            // Navigating to tagsFragment
+            navController.navigate(R.id.action_page_categories_to_tagsFragment);
+        });
+
     }
 }
