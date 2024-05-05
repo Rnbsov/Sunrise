129b94d985215b32b070992d7c85156f270c0b9d
Rnbsov <rnbsov@gmail.com>
2024-05-05 23:35:25 +0500

    🍃♻️ Add recycler view to categories fragment layout and make test setup

--- app/src/main/java/com/example/sunrise/fragments/CategoriesFragment.java
+++ app/src/main/java/com/example/sunrise/fragments/CategoriesFragment.java
@@ -1,6 +1,11 @@
 package com.example.sunrise.fragments;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -8,18 +13,20 @@ import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.sunrise.R;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.example.sunrise.adapters.CategoryAdapter;
+import com.example.sunrise.models.Category;
 
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CategoriesFragment extends Fragment {
 
+    private RecyclerView categoriesRecyclerView;
+    private CategoryAdapter categoryAdapter;
     private View fragment;
 
     public CategoriesFragment() {
@@ -51,6 +58,30 @@ public class CategoriesFragment extends Fragment {
             // Navigating to tagsFragment
             navController.navigate(R.id.action_page_categories_to_tagsFragment);
         });
+
+        // Initialize RecyclerView and adapter
+        categoriesRecyclerView = fragment.findViewById(R.id.categories_recycler_view);
+        categoriesRecyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
+        categoryAdapter = new CategoryAdapter(new ArrayList<>(), new CategoryAdapter.OnCategoryClickListener() {
+            @Override
+            public void onCategoryClick(Category category) {
+                // Handle category item click
+                Toast.makeText(requireContext(), "Clicked on category: " + category.getTitle(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Set adapter to RecyclerView
+        categoriesRecyclerView.setAdapter(categoryAdapter);
+
+        // Set categories to adapter
+
+// Get the resource ID of the drawable
+        int drawableResourceId = R.drawable.account_circle_24px;
+        List<Category> categories = new ArrayList<>();
+        categories.add(new Category("Category 1", Color.RED, drawableResourceId, "lalalal"));
+        categories.add(new Category("Category 2", Color.BLUE, drawableResourceId, "lalalal"));
+
+         categoryAdapter.setCategories(categories);
 
     }
 }
