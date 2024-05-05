15bf6503e0f85014b0466c2243677f14500d4596
Rnbsov <rnbsov@gmail.com>
2024-05-05 14:49:23 +0500

    🍋 Add new tags field to Task class

--- app/src/main/java/com/example/sunrise/models/Task.java
+++ app/src/main/java/com/example/sunrise/models/Task.java
@@ -1,9 +1,13 @@
 package com.example.sunrise.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task {
     private String title;
     private String priority;
     private String taskId;
+    private List<String> tags;
     private long createdAt;
     private long updatedAt;
     private String createdByUserId;
@@ -14,9 +18,10 @@ public class Task {
         // Default constructor required for calls to DataSnapshot.getValue(User.class)
     }
 
-    public Task(String title, String priority, String createdByUserId) {
+    public Task(String title, String priority, List<String> tags, String createdByUserId) {
         this.title = title;
         this.priority = priority;
+        this.tags = tags;
         this.createdAt = System.currentTimeMillis();
         this.updatedAt = System.currentTimeMillis(); // Set updatedAt to the same value as createdAt initially
         this.createdByUserId = createdByUserId;
@@ -25,6 +30,7 @@ public class Task {
     public Task(Task task) {
         this.title = task.title;
         this.priority = task.priority;
+        this.tags = new ArrayList<>(task.tags);;
         this.createdAt = task.createdAt;
         this.updatedAt = task.updatedAt;
         this.createdByUserId = task.createdByUserId;
@@ -96,5 +102,13 @@ public class Task {
 
     public void setCompletedAt(long completedAt) {
         this.completedAt = completedAt;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
     }
 }
