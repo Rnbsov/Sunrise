ab3cd3c9ab633cbaee816b573fa4d92450b97eec
Rnbsov <rnbsov@gmail.com>
2024-05-06 21:53:15 +0500

    🍸 Basic logic of `onCategoryAddButtonClick` listener's body

--- app/src/main/java/com/example/sunrise/fragments/CategoriesFragment.java
+++ app/src/main/java/com/example/sunrise/fragments/CategoriesFragment.java
@@ -1,10 +1,15 @@
 package com.example.sunrise.fragments;
 
+import android.content.Context;
+import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.PopupMenu;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -19,15 +24,38 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.sunrise.R;
 import com.example.sunrise.adapters.CategoryAdapter;
 import com.example.sunrise.models.Category;
+import com.example.sunrise.models.Tag;
+import com.example.sunrise.services.TagService;
+import com.example.sunrise.utils.ColorPickerDialog;
+import com.example.sunrise.utils.IconPickerDialog;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.chip.Chip;
+import com.google.android.material.imageview.ShapeableImageView;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class CategoriesFragment extends Fragment {
 
     private RecyclerView categoriesRecyclerView;
     private CategoryAdapter categoryAdapter;
     private View fragment;
+    private ShapeableImageView setIcon;
+    private TextInputLayout titleInputLayout;
+    private TextInputEditText editTitle;
+    private Chip defaultTagChip;
+    private int selectedIconId;
+    private String selectedTagId;
+    private int selectedColor;
+    private ColorPickerDialog colorPickerDialog;
+    private IconPickerDialog iconPickerDialog;
 
     public CategoriesFragment() {
         // Required empty public constructor
@@ -68,7 +96,7 @@ public class CategoriesFragment extends Fragment {
                 // Handle category item click
                 Toast.makeText(requireContext(), "Clicked on category: " + category.getTitle(), Toast.LENGTH_SHORT).show();
             }
-        });
+        }, this::onCategoryAddButtonClick);
 
         // Set adapter to RecyclerView
         categoriesRecyclerView.setAdapter(categoryAdapter);
@@ -83,5 +111,41 @@ public class CategoriesFragment extends Fragment {
 
          categoryAdapter.setCategories(categories);
 
+    }
+
+    private void onCategoryAddButtonClick(View v) {
+        Context context = requireContext();
+        View fragmentRootView = requireView(); // Get the root view of the fragment
+
+        // Setup bottom sheet dialog
+        BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(context);
+        View bottomSheetContentView = LayoutInflater.from(context).inflate(R.layout.create_category_bottom_sheet, (ViewGroup) fragmentRootView, false);
+        bottomSheetDialog.setContentView(bottomSheetContentView);
+        bottomSheetDialog.show();
+
+        // Find views
+        setIcon = bottomSheetContentView.findViewById(R.id.set_icon);
+        titleInputLayout = bottomSheetContentView.findViewById(R.id.category_title_input_layout);
+        editTitle = bottomSheetContentView.findViewById(R.id.title);
+        defaultTagChip = bottomSheetContentView.findViewById(R.id.default_tag);
+        Button createBtn = bottomSheetContentView.findViewById(R.id.create_btn);
+
+        // Setting on setIcon click listener
+        setIcon.setOnClickListener(this::onSetIconClick);
+
+        // Setting on defaultTagChip click listener
+        defaultTagChip.setOnClickListener(this::showTagsPopupMenu);
+
+        // Setting listener on save button
+        createBtn.setOnClickListener(view -> createCategory());
+    }
+
+    private void onSetIconClick(View v) {
+    }
+
+    private void showTagsPopupMenu(View v) {
+    }
+
+    private void createCategory() {
     }
 }
