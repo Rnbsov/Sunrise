bfc24687553f66efd7a71d4a72be3d0cb1ab5072
Rnbsov <rnbsov@gmail.com>
2024-05-20 03:19:10 +0500

    🔥 List tasks belonging to category

--- app/src/main/java/com/example/sunrise/fragments/CategoryFragment.java
+++ app/src/main/java/com/example/sunrise/fragments/CategoryFragment.java
@@ -1,22 +1,36 @@
 package com.example.sunrise.fragments;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.sunrise.R;
+import com.example.sunrise.adapters.TasksAdapter;
+import com.example.sunrise.helpers.TaskListenerHelper;
+import com.example.sunrise.models.Task;
+import com.example.sunrise.services.TaskService;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class CategoryFragment extends Fragment {
 
+    private RecyclerView tasksRecyclerView;
+    private TasksAdapter taskAdapter;
+    private TaskService taskService;
     private String categoryId;
     private String categoryTitle;
 
@@ -43,5 +57,43 @@ public class CategoryFragment extends Fragment {
 
         // Set action bar title to category title
         Objects.requireNonNull(((AppCompatActivity) requireActivity()).getSupportActionBar()).setTitle(categoryTitle);
+
+        // Initialize TaskService to interact with Firebase database
+        taskService = new TaskService();
+
+        // Initialize TaskListenerHelper to help with responding task actions
+        TaskListenerHelper taskListenerHelper = new TaskListenerHelper(requireContext());
+
+        tasksRecyclerView = view.findViewById(R.id.tasks_recycler_view);
+        tasksRecyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
+        taskAdapter = new TasksAdapter(new ArrayList<>(), taskListenerHelper::onCheckboxClickedListener, taskListenerHelper::onTaskClickListener);
+        tasksRecyclerView.setAdapter(taskAdapter);
+
+        fetchTasksFromDatabase();
+    }
+
+    private void fetchTasksFromDatabase() {
+        ValueEventListener tasksListener = new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                List<Task> taskList = new ArrayList<>();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Task task = snapshot.getValue(Task.class);
+                    if (task != null && task.getCategoryId().equals(categoryId)) {
+                        taskList.add(task);
+                    }
+                }
+
+                // Update data in the adapter using DiffUtil
+                taskAdapter.updateData(taskList);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.d("CategoryFragment", "Failed to load tasks");
+            }
+        };
+        taskService.getTasksByCategoryId(categoryId, tasksListener);
     }
 }
