965eab63e7f7b115ab532c408c2302c165de5443
Rnbsov <rnbsov@gmail.com>
2024-05-19 22:45:48 +0500

    🍃 Create category fragment and layout for it

---
+++ app/src/main/java/com/example/sunrise/fragments/CategoryFragment.java
@@ -0,0 +1,25 @@
+package com.example.sunrise.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.sunrise.R;
+
+public class CategoryFragment extends Fragment {
+
+    public CategoryFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_category, container, false);
+    }
+}
