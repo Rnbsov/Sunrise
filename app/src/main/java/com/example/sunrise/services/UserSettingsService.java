601abb86df24df838ef15b6642c73451f9f645e0
Rnbsov <rnbsov@gmail.com>
2024-05-20 22:38:40 +0500

    🍊 Create User Settings model and service

---
+++ app/src/main/java/com/example/sunrise/services/UserSettingsService.java
@@ -0,0 +1,41 @@
+package com.example.sunrise.services;
+
+import com.example.sunrise.models.UserSettings;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
+
+public class UserSettingsService {
+    private final DatabaseReference userSettingsRef;
+
+    public UserSettingsService() {
+        // Initialize Firebase database reference
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        this.userSettingsRef = database.getReference("UserSettings");
+    }
+
+    public void getUserSettings(ValueEventListener listener) {
+        // Get currently logged-in user Id
+        String userId = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
+
+        // Create a query to get user's settings by userId
+        Query query = userSettingsRef.orderByChild("createdByUserId").equalTo(userId);
+
+        // Add SingleValueEventListener to the query to retrieve settings object once
+        query.addListenerForSingleValueEvent(listener);
+    }
+
+    public void createUserSettings(UserSettings userSettings) {
+        // Generate a reference to a new child location under "UserSettings" with a client-side auto-generated key
+        DatabaseReference newUserSettingsRef = userSettingsRef.push();
+
+        String userSettingsId = newUserSettingsRef.getKey(); // Retrieve the unique ID
+        userSettings.setId(userSettingsId); // Save this unique ID to the UserSettings object
+
+        newUserSettingsRef.setValue(userSettings); // Save the UserSettings to Firebase database
+    }
+}
