cae85e05b00c6de65285bcfb001e71abea2cfb98
Rnbsov <rnbsov@gmail.com>
2024-05-20 13:57:38 +0500

    🍃 Update task category logic

--- app/src/main/java/com/example/sunrise/helpers/TaskListenerHelper.java
+++ app/src/main/java/com/example/sunrise/helpers/TaskListenerHelper.java
@@ -2,36 +2,48 @@ package com.example.sunrise.helpers;
 
 import android.content.Context;
 import android.graphics.Paint;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+
 import com.example.sunrise.R;
+import com.example.sunrise.models.Category;
 import com.example.sunrise.models.Task;
+import com.example.sunrise.services.CategoryService;
 import com.example.sunrise.services.TaskService;
 import com.example.sunrise.utils.TaskUtils;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.chip.Chip;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Objects;
 
 public class TaskListenerHelper {
     private final Context context;
     private final TaskService taskService;
+    private final CategoryService categoryService;
     private BottomSheetDialog bottomSheetDialog;
     private TextInputLayout titleInputLayout;
     private TextInputEditText editTitle;
     private Chip priorityChip;
+    private Chip categoryChip;
     private Button saveBtn;
+    private String selectedCategoryId;
 
     public TaskListenerHelper(Context context) {
         this.context = context;
 
-        // Initialize TaskService and TagService to interact with Firebase database
+        // Initialize TaskService and Category to interact with Firebase database
         this.taskService = new TaskService();
+        this.categoryService = new CategoryService();
     }
 
     public void onTaskClickListener(Task task) {
@@ -50,12 +62,26 @@ public class TaskListenerHelper {
         // Setting properties from task object
         editTitle.setText(task.getTitle());
         priorityChip.setText(task.getPriority());
+        // Fetch and set the category name
+        fetchAndSetCategory(task.getCategoryId());
 
         // Change priority listener
         priorityChip.setOnClickListener(view -> TaskUtils.showPriorityPopupMenu(context, view, priorityChip));
 
+        // Change category listener
+        categoryChip = bottomSheetContentView.findViewById(R.id.category);
+        categoryChip.setOnClickListener(view -> TaskUtils.showCategoriesPopupMenu(context, view, categoryChip, categoryService, this::onCategorySelected));
+
         // Setting listener on save button
         saveBtn.setOnClickListener(view -> saveTask(task));
+    }
+
+    private void onCategorySelected(String selectedCategoryId, String selectedCategoryName) {
+        // Store it like class var
+        this.selectedCategoryId = selectedCategoryId;
+
+        // Set selected category's title to chip
+        categoryChip.setText(selectedCategoryName);
     }
 
     private void saveTask(Task task) {
@@ -74,6 +100,7 @@ public class TaskListenerHelper {
         // Update copy with user-entered data
         updatedTask.setTitle(title);
         updatedTask.setPriority(priority);
+        updatedTask.setCategoryId(selectedCategoryId);
 
         // Save updated task to Firebase database
         taskService.updateTask(updatedTask);
@@ -103,5 +130,24 @@ public class TaskListenerHelper {
             // If task is not completed, it shouldn't be applied strikethrough style
             title.setPaintFlags(title.getPaintFlags() & (~Paint.STRIKE_THRU_TEXT_FLAG));
         }
+    }
+
+
+    private void fetchAndSetCategory(String categoryId) {
+        categoryService.getCategoryById(categoryId, new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Category category = snapshot.getValue(Category.class);
+                if (category != null) {
+                    selectedCategoryId = category.getCategoryId();
+                    categoryChip.setText(category.getTitle());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e("TaskListenerHelper", "Failed to get task's category object");
+            }
+        });
     }
 }
